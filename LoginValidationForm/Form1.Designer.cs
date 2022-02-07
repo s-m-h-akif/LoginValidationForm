9dc7dba24c525a4c6faee5cb9148af034e6febe0
Syed Akif <smhakkif@gmail.com>
2022-02-08 01:48:53 +0530

    Add project files.

---
+++ LoginValidationForm/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace LoginValidationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_login = new System.Windows.Forms.Button();
+            this.label_passwordWarning = new System.Windows.Forms.Label();
+            this.label_nameWarning = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password";
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(390, 107);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(293, 22);
+            this.textBox_name.TabIndex = 2;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(390, 193);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(293, 22);
+            this.textBox_password.TabIndex = 3;
+            // 
+            // button_login
+            // 
+            this.button_login.Location = new System.Drawing.Point(361, 275);
+            this.button_login.Name = "button_login";
+            this.button_login.Size = new System.Drawing.Size(129, 45);
+            this.button_login.TabIndex = 4;
+            this.button_login.Text = "LOGIN";
+            this.button_login.UseVisualStyleBackColor = true;
+            this.button_login.Click += new System.EventHandler(this.button_login_Click);
+            // 
+            // label_passwordWarning
+            // 
+            this.label_passwordWarning.AutoSize = true;
+            this.label_passwordWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_passwordWarning.ForeColor = System.Drawing.Color.Red;
+            this.label_passwordWarning.Location = new System.Drawing.Point(387, 174);
+            this.label_passwordWarning.Name = "label_passwordWarning";
+            this.label_passwordWarning.Size = new System.Drawing.Size(13, 16);
+            this.label_passwordWarning.TabIndex = 5;
+            this.label_passwordWarning.Text = "*";
+            this.label_passwordWarning.Visible = false;
+            // 
+            // label_nameWarning
+            // 
+            this.label_nameWarning.AutoSize = true;
+            this.label_nameWarning.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_nameWarning.ForeColor = System.Drawing.Color.Red;
+            this.label_nameWarning.Location = new System.Drawing.Point(386, 88);
+            this.label_nameWarning.Name = "label_nameWarning";
+            this.label_nameWarning.Size = new System.Drawing.Size(13, 16);
+            this.label_nameWarning.TabIndex = 6;
+            this.label_nameWarning.Text = "*";
+            this.label_nameWarning.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.label_nameWarning);
+            this.Controls.Add(this.label_passwordWarning);
+            this.Controls.Add(this.button_login);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_login;
+        private System.Windows.Forms.Label label_passwordWarning;
+        private System.Windows.Forms.Label label_nameWarning;
+    }
+}
+
---
+++ LoginValidationForm/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginValidationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_login_Click(object sender, EventArgs e)
+        {
+            if(textBox_name.Text == " ")
+            {
+                label_nameWarning.Visible = true;
+                MessageBox.Show("Enter UserName");
+            }
+            if(textBox_password.Text == " ")
+            {
+                label_passwordWarning.Visible = true;
+                MessageBox.Show("Enter password");
+            }
+            else if(!Regex.Match(textBox_name.Text,"^[a-zA-Z]*$").Success)
+            {
+                MessageBox.Show("Number is not Allowed");
+                textBox_name.Focus();
+            }
+            else if(textBox_password.Text == "admin")
+            {
+                label_nameWarning.Visible = false;
+                label_passwordWarning.Visible=false;
+
+                this.Hide();
+                MdiForm mdi = new MdiForm();
+                mdi.Show();
+
+            }
+        }
+    }
+}
